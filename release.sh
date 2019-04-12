5d35e487c03534ae265ea4d7b1d4f59b2c5438d3
yazgoo <yazgoo@gmail.com>
2019-04-12 06:55:40 +0200

    fix release script

--- release.sh
+++ release.sh
@@ -1,3 +1,8 @@
+set -xe
 version=$(grep VERSION lib/version.rb| cut -d \" -f2)
-git commit -am $version && git tag $version && git push --tags && git push && gem build term-slides.gemspec && gem push term-slides-$version.gem
-
+git commit -am "$*" 
+git push 
+git tag $version 
+git push --tags 
+gem build term-slides.gemspec 
+gem push term-slides-$version.gem
